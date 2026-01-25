df8fe8f5fdf20b90a36dc240cad7d88cb0e677fa
arielarmoni-ux <arielarmoni@gmail.com>
2026-01-25 22:12:02 +0200

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,4 +1,4 @@
-const baseURL = "https://arielarmoni-ux.github.io/my-portfolio";
+const baseURL = "https://arielarmoni-ux.github.io/my-portfolio-2";
 
 async function injectSidebar() {
     if (document.getElementById('side-nav')) return;
